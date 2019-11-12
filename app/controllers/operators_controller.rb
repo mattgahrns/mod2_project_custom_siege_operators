419588954b3d0f21d14d1d4eb9c0385c2f155484
Matt Gahrns <matthewgahrns@hotmail.com>
2019-11-12 13:48:30 -0800

    More work on _form

--- app/controllers/operators_controller.rb
+++ app/controllers/operators_controller.rb
@@ -6,6 +6,15 @@ class OperatorsController < ApplicationController
 
     def new
         @operator = Operator.new
+        @attk_gadgets = Gadget.where(attacker: true).order(:name)
+        @def_gadgets = Gadget.where(attacker: false).order(:name)
+        @primary_weapons = Weapon.where(primary: true).order(:name)
+        @secondary_weapons = Weapon.where(primary: false).order(:name)
+        @attk_utility = Utility.where(attacker: true).order(:name)
+        @def_utility = Utility.where(attacker: false).order(:name)
+        @sights = Sight.all
+        @barrels = Barrel.all
+        @grips = Grip.all
     end
 
     def create
