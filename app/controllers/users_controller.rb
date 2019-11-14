03394d37f53a46aefe891b973000af70800bd9d6
Matt Gahrns <matthewgahrns@hotmail.com>
2019-11-13 17:57:15 -0800

    Work on edit and delete for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,62 @@
 class UsersController < ApplicationController
 
+    before_action :authorize, only: [:edit, :delete]
+
     def new
         @user = User.new
     end
 
     def create
-        if @user = User.create(user_params)
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
             flash[:notice] = "Account created successfully!"
+            session[:user_id] = @user.id.to_s
             redirect_to user_path(@user)
         else
-            flash.now[:error] = "Username already taken please choose a different one!"
+            flash[:error] = "Username already taken please choose a different one!"
             render :new
         end
     end
 
     def edit
         @user = User.find(params[:id])
+        if @user.id.to_s != session[:user_id]
+            flash[:notice] = "You can't edit other users!"
+            redirect_to user_path(@user)
+        end
     end
 
     def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            flash[:notice] = "Account details updated successfully!"
+            redirect_to user_path(@user)
+        else
+            flash.now[:error] = "Username already taken please choose a different one!"
+            render :edit
+        end
 
     end
 
     def show
         @user = User.find(params[:id])
+    end
+
+    def delete
+        @user = User.find(params[:id])
+        if @user.id.to_s != session[:user_id]
+            flash[:notice] = "You can't delete other users!"
+            redirect_to user_path(@user)
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        session.delete(:user_id)
+        redirect_to login_path, notice: "Logged out!"
     end
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, uniqueness: true
     validates :username, presence: true
-    has_many :operators
+    has_many :operators, dependent: :destroy
     has_secure_password
 end
